5d422f1c4828fa659c44df43cb378fcd73aa6f11
ken1flan <ken1flan@gmail.com>
2023-05-17 12:06:25 +0900

    blorgh/application.cssの参照エラーの修正
    参考
    https://muramurasan.hatenablog.jp/entry/2017/02/12/221810

--- blorgh/lib/blorgh/engine.rb
+++ blorgh/lib/blorgh/engine.rb
@@ -1,5 +1,12 @@
 module Blorgh
   class Engine < ::Rails::Engine
     isolate_namespace Blorgh
+
+    initializer "blorgh.assets.precompile", group: :all do |app|
+      app.config.assets.precompile += %w(
+        blorgh/application.js
+        blorgh/application.css
+      )
+    end
   end
 end
